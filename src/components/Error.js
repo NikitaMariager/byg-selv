7320f8159770f353107bca27e92da02e7d7cc59d
Nikita Mariager <101179357+NikitaMariager@users.noreply.github.com>
2022-12-06 11:04:09 +0100

    Delete src directory

--- src/components/Error.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Error() {
-  return <h2>der er sket en fejl</h2>;
-}
-
-export default Error;
--- src/pages/Notfound.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Notfound() {
-  return <div> Page not found</div>;
-}
-
-export default Notfound;
